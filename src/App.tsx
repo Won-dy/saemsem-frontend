a6173746820c302f78bde98040b0d4ea35f4d6dc
원동연 <junhfireace@gmail.com>
2024-01-23 12:22:42 +0900

    feat: Router 구조 잡기

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import './App.css';
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainHeader from './common/components/mainHeader/MainHeader';
-import MainBody from './common/components/mainBody/MainBody';
+import GNB from './common/components/gnb/GNB';
+import Home from './pages/home/Home';
+import './App.css';
 
 function App() {
+  const [use100Width, setUse100Width] = useState<boolean>(false);
+  const [currentPath, setCurrentPath] = useState<string>(window.location.pathname);
+  useEffect(() => {
+    const newPath = window.location.pathname;
+    setUse100Width(newPath.includes('login') || newPath.includes('404'));
+    if (newPath !== currentPath) {
+      setCurrentPath(newPath);
+    }
+  }, [currentPath]);
+
   return (
     <div className='App'>
-      <MainHeader />
-      <MainBody />
+      <BrowserRouter>
+        <MainHeader />
+        <div id='main-body'>
+          <GNB />
+          <div id='main-content-wrap' style={{ width: use100Width ? '100%' : '1000px' }}>
+            <Routes>
+              <Route path='/' element={<Navigate to='/login' replace />} />
+              <Route path='/home' element={<Home />} />
+              <Route path='/login' element={<>Login</>} />
+              <Route path='/login/join' element={<>Join</>} />
+              <Route path='/login/join' element={<>Join</>} />
+              <Route path='/404' element={<>Not Found</>} />
+              <Route path='*' element={<Navigate to='/404' />} />
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
